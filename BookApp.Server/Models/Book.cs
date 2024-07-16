3eb50bf47df1c4dc3f0fc2de2e83d2450da6632b
Mike <michaelokolo62@yahoo.com>
2024-07-16 23:57:43 +0100

    Add project files.

---
+++ BookApp.Server/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookApp.Server.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public int YearPublished { get; set; }
+    }
+}
