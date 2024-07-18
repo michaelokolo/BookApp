c2a9a068981a3ff017923693668611fda557fa47
Mike <michaelokolo62@yahoo.com>
2024-07-18 02:41:35 +0100

    completed the form for the create page

--- bookapp.client/src/pages/CreateBook.jsx
+++ bookapp.client/src/pages/CreateBook.jsx
@@ -1,7 +1,68 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'
+
+
 function CreateBook() {
-  return (
-    <p>Create Book</p>
+    return (
+        <Container className="mt-5">
+            <Row className="justify-content-md-center">
+                <Col xs={12} md={6}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="title">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Title"
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="author">
+                            <Form.Label>Author</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Author"
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="price">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Price"
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="yearPublish">
+                            <Form.Label>Year</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Year"
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="description">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                placeholder="Enter a brief description of the book"
+                            />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+                
+            </Row>
+            
+      </Container>
+    
   );
 }
 
 export default CreateBook;
+
