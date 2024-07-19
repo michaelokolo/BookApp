f239a5f6bcc10900d1301367308687473ca7f754
Mike <michaelokolo62@yahoo.com>
2024-07-19 01:20:06 +0100

    completed the create book form functionality

--- bookapp.client/src/pages/CreateBook.jsx
+++ bookapp.client/src/pages/CreateBook.jsx
@@ -11,21 +11,41 @@ function CreateBook() {
         title: '',
         author: '',
         price: 0,
-        yearPublish: 0,
-        description:'',
+        description: '',
+        yearPublished: 0,
     });
-    const handleChange = (e) => {
+    const handleChange =  (e) => {
         setFormData({
             ...formData,
             [e.target.id]: e.target.value,
         });
     };
     console.log(formData);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/books', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+        
+       
+        
+    };
     return (
         <Container className="mt-5">
             <Row className="justify-content-md-center">
                 <Col xs={12} md={6}>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="title">
                             <Form.Label>Title</Form.Label>
                             <Form.Control
@@ -49,19 +69,19 @@ function CreateBook() {
                         <Form.Group className="mb-3" controlId="price">
                             <Form.Label>Price</Form.Label>
                             <Form.Control
-                                type="text"
+                                type="number"
                                 placeholder="Price"
                                 value={formData.price}
                                 onChange={handleChange}
                             />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="yearPublish">
+                        <Form.Group className="mb-3" controlId="yearPublished">
                             <Form.Label>Year</Form.Label>
                             <Form.Control
-                                type="text"
+                                type="number"
                                 placeholder="Year"
-                                value={formData.yearPublish}
+                                value={formData.yearPublished}
                                 onChange={handleChange}
                             />
                         </Form.Group>
