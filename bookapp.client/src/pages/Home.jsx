6ff76124c1506626ed2c7677fb252230e03b7b49
Mike <michaelokolo62@yahoo.com>
2024-07-26 03:51:58 +0100

    connected the home page to the get all books api

--- bookapp.client/src/pages/Home.jsx
+++ bookapp.client/src/pages/Home.jsx
@@ -1,6 +1,57 @@
+import { useState,useEffect } from "react";
+
+
 function Home() {
-  return (
-    <p>Home</p>
+    const [books, setBooks] = useState(null);
+
+
+    useEffect(() => {
+        const fetchbooks = async () => {
+            const res = await fetch('/api/books');
+            if (!res.ok) {
+                throw new Error(`Server responded with a ${res.status}`);
+            }
+            const data = await res.json();
+            console.log(data);
+            setBooks(data);
+        }
+        fetchbooks();
+    }, []);
+    
+    return (
+        <div className="container mt-5">
+        <h1 className="text-center">Book List</h1>
+            <table className="table table-hover table-responsive">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Year Published</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        books && books.map(book => (
+                            <tr key={book.id}>
+                                <th  scope="row">{book.id}</th>
+                                <td>{book.title}</td>
+                                <td>{book.description}</td>
+                                <td>{book.author}</td>
+                                <td>{book.price}</td>
+                                <td>{book.yearPublished}</td>
+                            </tr>
+                        ))
+                    }
+                        
+                    
+                    
+                </tbody>
+            </table>
+        </div>
+        
   );
 }
 
