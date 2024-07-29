a81033d989932b15bcbfa250a93a721b4a939b03
Mike <michaelokolo62@yahoo.com>
2024-07-29 03:46:59 +0100

    Changed the header and included a search feature and button

--- bookapp.client/src/components/Header.jsx
+++ bookapp.client/src/components/Header.jsx
@@ -1,31 +1,42 @@
-import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar';
 import { FcReadingEbook } from "react-icons/fc";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
 
 
 function Header() {
-  return (
-      <Card>
-          <Card.Header>
-              <Nav variant="tabs" defaultActiveKey="/">
-                  
-                  <Nav.Item>
-                      <Nav.Link as={Link} to="/"><FcReadingEbook size={30} /></Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                      <Nav.Link as={Link} to="/">Home</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                      <Nav.Link as={Link} to="/create-book">Create Book</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                      <Nav.Link as={Link} to="/about">About</Nav.Link>
-                  </Nav.Item>
-
-              </Nav>
-          </Card.Header>
-      </Card>
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/"><FcReadingEbook /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link as={ Link } to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/create-book">Create Book</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
   );
 }
 
