ecb7e8b3fc3a860b7c049a9168e3480e94a3bab5
Mike <michaelokolo62@yahoo.com>
2024-08-01 23:46:02 +0100

    Set up the upload API

--- BookApp.Server/Controllers/BooksController.cs
+++ BookApp.Server/Controllers/BooksController.cs
@@ -2,21 +2,58 @@
 using Microsoft.AspNetCore.Mvc;
 using BookApp.Server.Data;
 using Microsoft.EntityFrameworkCore;
-
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 
 namespace BookApp.Server.Controllers
 {
+    
+
     [Route("api/[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
     {
+        string loggedInUser = "matt"; // the name matt will eventually be replaced by the logged in user
+
         private readonly BookContext _context;
 
-        public BooksController(BookContext context )
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BooksController(BookContext context, BlobServiceClient blobServiceClient)
         {
             _context = context;
+            _blobServiceClient = blobServiceClient;
             
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if(file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            if(file.Length > 204800)// 200 KB
+            {
+                return BadRequest("File size exceeds the maximum allowed size of 200 KB.");
+            }
+
+            var containerClient = _blobServiceClient.GetBlobContainerClient(loggedInUser);
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+            var blobClient = containerClient.GetBlobClient(file.FileName);
+
+            // Upload the file to the blob storage
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, overwrite: true);
+            }
+
+            // Return the URL of the uploaded blob
+            return Ok(new { url = blobClient.Uri.ToString() });
+
         }
 
         [HttpGet]
