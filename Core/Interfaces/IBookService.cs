607c8caa9c0184fb054c9f6f417bcab286238640
Mike <michaelokolo62@yahoo.com>
2025-02-10 23:18:35 +0000

    set up the book service interface

---
+++ Core/Interfaces/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using BookApp.Core.Models;
+
+namespace BookApp.Core.Interfaces
+{
+    internal interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+        Task<Book> GetBookByIdAysnc(int id);
+        Task<Book> AddBookAsync(Book book);
+        Task<Book> UpdateBookAsync(int id, Book book);
+        Task<bool> DeleteBookAsync(int id);
+    }
+}
