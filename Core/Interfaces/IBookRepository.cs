cafcead87e2dcabc2e96ee3ec8acc0580f21a82d
Mike <michaelokolo62@yahoo.com>
2025-02-10 23:26:29 +0000

    Completed the book repository interface

---
+++ Core/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookApp.Core.Models;
+
+
+namespace BookApp.Core.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+        Task<Book> GetBookByIdAsync(int id);
+        Task<Book> AddBookAsync(Book book);
+        Task<Book> UpdateBookAsync(Book book);
+        Task<bool> DeleteBookAsync(int id);
+    }
+}
